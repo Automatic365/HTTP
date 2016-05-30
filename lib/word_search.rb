3fff4eb7a6d32bfc041aca29b3b88af2cf7d52c9
Jason Hanna <jason.hanna01@gmail.com>
2016-05-30 14:15:11 -0600

    added word search method

--- lib/word_search.rb
+++ lib/word_search.rb
@@ -2,8 +2,17 @@ require 'pry'
 
 class WordSearch
   attr_reader :dictionary
+
   def initialize
     @dictionary = File.read("/usr/share/dict/words").split("\n")
+  end
+
+
+  def valid_word?(word)
+    if word
+      word = word.downcase
+    end
+    dictionary.include?(word)
   end
 
 end
