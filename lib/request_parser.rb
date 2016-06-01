34dcf32e4790b6184eb4c86c5648b4b52554e043
riskybiscuit <patrick.w.wentz@gmail.com>
2016-06-01 07:51:24 -0600

    deleted unused local variables

--- lib/request_parser.rb
+++ lib/request_parser.rb
@@ -8,38 +8,37 @@ class RequestParser
   end
 
   def get_verb
-    verb = @request_lines.first.split.first
+    @request_lines.first.split.first
   end
 
   def get_path
-    path = @request_lines.first.split[1]
+    @request_lines.first.split[1]
   end
 
   def get_protocol
-    protocol = @request_lines.first.split.last
-    @request["Protocol:"] = protocol
+    @request_lines.first.split.last
   end
 
   def get_host
-    host = @request_lines.find_all do |line|
+    @request_lines.find_all do |line|
       line.include?("Host")
     end.join.split(": ").last
   end
 
   def get_port
-    port = @request_lines.find_all do |line|
+    @request_lines.find_all do |line|
       line.include?("Host")
     end.join.split(":").last
   end
 
   def get_origin
-    origin = @request_lines.find_all do |line|
+    @request_lines.find_all do |line|
       line.include?("Host")
     end.join.split(":")[1].lstrip
   end
 
   def get_accept
-    accept = @request_lines.find do |line|
+    @request_lines.find do |line|
       line.split(":").include?("Accept")
     end.split(": ").last
   end
